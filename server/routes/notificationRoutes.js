646c8f99d05febc630cccbf3724b8da73fe25c17
KillaB <sugam@beeaver.co>
2025-08-05 11:50:18 +0545

    fix: only admin can create notification

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import { createNotification, getNotifications, openNotification } from "../controllers/notificationController.js";
 
 const router = express.Router();
 
 router.get("/",requireSignIn, getNotifications);
-router.post("/",requireSignIn, createNotification);
+router.post("/",requireSignIn,isAdmin, createNotification);
 router.get("/:id",requireSignIn, openNotification);
 
 export default router
