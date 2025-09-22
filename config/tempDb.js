27dfec03d97c4aa2c3f75893fd654dbef3047380
KillaB <sugam@beeaver.co>
2025-09-22 13:35:22 +0545

    fix: removed env file for security

--- config/tempDb.js
+++ config/tempDb.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("whitefeat_wf_new","white_feat_dev","P@ssw00rd",{
-host     : 'localhost',
+export const sequelize = new Sequelize("whitefeat_wf_new","killerbee_local","P@ssw00rd",{
+host     : '3.110.62.209',
 dialect : 'mysql'
 }); 
 
