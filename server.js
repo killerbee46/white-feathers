c7cfdd09651d9a39de62d58c5a88194a29d34aab
KillaB <sugam@beeaver.co>
2025-12-07 11:37:21 +0545

    feat: control back to autobots

--- server.js
+++ server.js
@@ -57,11 +57,11 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api",apiRoutes);
 app.use("/upload", uploadRoutes)
 
-// schedule.scheduleJob({ hour: 11, minute: 11, tz: "Asia/Kathmandu" }, async function () {
+schedule.scheduleJob({ hour: 11, minute: 11, tz: "Asia/Kathmandu" }, async function () {
   const rates = await fetchTodaysGoldSilverRates();
   updateMaterialPrice(rates)
   updateCurrency()
-// });
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
