86d3f9dd7971cbf5afd1f93f4eb63526f64873e7
KillaB <sugam@beeaver.co>
2026-01-21 16:56:07 +0545

    feat: order list by user

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -84,17 +84,47 @@ export const getOrdersByUser = async (req, res) => {
     try {
         const userId = req?.user?.id
 
-        const ord = await Order.findAll({ where: { $and: [{ c_id: userId }, { checkout: { $not: 0 } }] } })
+        const ord = await Order.findAll({ where: { [Op.and]: [{ c_id: userId }, { checkout: { [Op.ne]: 0 } }] },
+        include: [
+                {
+                    model: CartDetails,
+                    include: [
+                        {
+                            model: Product,
+                            attributes: ['image', 'p_name'],
+                            include: [PackageSlider]
+                        },
+                    ]
+                }
+            ] })
 
         const ordDetails = ord?.map((o) => {
             let totalPrice = 0
             let pdiscount = 0
-            const products = JSON.parse(o?.cookie_id)?.products ?? []
-            products?.map((p) => {
-                totalPrice += (p.dynamic_price * p.quantity + p.discount * p.quantity);
-                pdiscount += p.discount * p.quantity;
+            const temp = JSON.parse(o?.cookie_id)?.products ?? o?.CartDetails
+            const products = temp?.map((p) => {
+                const price = p.dynamic_price ?? p.rate
+                const quantity = p.quantity ?? p.qty
+                totalPrice += (price * quantity + p.discount * quantity);
+                pdiscount += p.discount * quantity;
+                return {
+                    id: p?.id ?? p?.id_pack,
+                    title: p?.title ?? p?.Product?.p_name,
+                    image: p?.image ?? p?.Product?.image ?? p?.Product?.Images[0]?.s_path,
+                    dynamic_price: price,
+                    quantity: quantity
+                }
             })
             return {
+                name: o?.name,
+                address: o?.address,
+                cno: o?.cno,
+                book_date: o?.book_date,
+                p_date: o?.p_date,
+                mode: o?.mode,
+                dispatch: o?.dispatch,
+                deliver: o?.deliver,
+                checkout: o?.checkout,
                 products: products,
                 totalPrice: totalPrice,
                 totalDiscount: pdiscount,
