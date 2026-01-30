13312ad052ed2f7fc1e489ea5a087788a65c0035
KillaB <sugam@beeaver.co>
2026-01-30 09:49:57 +0545

    fix: db config

--- config/tempDb.js
+++ config/tempDb.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('whitefeat_wf_new','root','',{
+export const sequelize = new Sequelize('whitefeat_wf_new','whitefeat_killerbee','P@ssw00rd',{
 host     : 'localhost',
 dialect : 'mysql'
 }); 
