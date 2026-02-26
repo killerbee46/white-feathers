df23293eb804f4a458c11008404a282028c663e7
KillaB <sugam@beeaver.co>
2026-02-26 12:43:53 +0545

    feat: update prices from api

--- utils/updateMaterialPrice.js
+++ utils/updateMaterialPrice.js
@@ -7,15 +7,15 @@ export const updateMaterialPrice = (rates) => {
         let updateRateQuery = ""
         switch (key) {
           case "gold":
-            updateRateQuery = `update package_material set price = ${materialRates[key]}, last_updated_at = '${dayjs()}' where pm_id = 2`
+            updateRateQuery = `update package_material set price = ${materialRates[key]}, last_updated_at = '${dayjs()}' where pm_id = 4`
             sequelize.query(updateRateQuery)
             break
-          case "silver":
-            updateRateQuery = `update package_material set price = ${materialRates[key]}, last_updated_at = '${dayjs()}' where pm_id = 3`
-            sequelize.query(updateRateQuery)
-            updateRateQuery = `update package set fixed_price = ${materialRates[key]+250} where id_pack = 2292`
-            sequelize.query(updateRateQuery)
-            break
+          // case "silver":
+          //   updateRateQuery = `update package_material set price = ${materialRates[key]}, last_updated_at = '${dayjs()}' where pm_id = 3`
+          //   sequelize.query(updateRateQuery)
+          //   updateRateQuery = `update package set fixed_price = ${materialRates[key]+250} where id_pack = 2292`
+          //   sequelize.query(updateRateQuery)
+          //   break
           default:
             break;
         }
